4b36364458a3dbc2cc14027817ff52dbcec999a3
KuanLin Chen <kuanchen83@gmail.com>
2017-12-08 15:37:50 -0800

    Update LogInPage.java

--- src/LogInPage.java
+++ src/LogInPage.java
@@ -1,8 +1,4 @@
 
-
-
-
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Date;
@@ -21,23 +17,6 @@ public class LogInPage extends JFrame {
 	private JPanel logInMainPanel;
 	private JTextField userNameText;
 	private JTextField passwordText;
-
-
-//	/**
-//	 * Launch the application.
-//	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					LogInPage frame = new LogInPage();
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
 
 	/**
 	 * Create the frame.
@@ -98,7 +77,6 @@ public class LogInPage extends JFrame {
 					}
 				}
 			}
-			
 		});
 		
 		JButton registerButton = new JButton("Register");
@@ -110,7 +88,6 @@ public class LogInPage extends JFrame {
 				RegisterPage reg = new RegisterPage(model);
 				dispose();
 			}
-			
 		});
 		
 		setVisible(true);
