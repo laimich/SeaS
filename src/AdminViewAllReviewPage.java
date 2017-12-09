a9b4b970812b8975767eab9248d0365a5fc7cb66
KuanLin Chen <kuanchen83@gmail.com>
2017-12-08 18:44:29 -0800

    I took away //home()

--- src/AdminViewAllReviewPage.java
+++ src/AdminViewAllReviewPage.java
@@ -1,5 +1,4 @@
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
@@ -21,21 +20,6 @@ public class AdminViewAllReviewPage extends JFrame {
 	private JPanel contentPane;
 	private JTable waterbodyInfoTable;
 	private WaterModel model;
-	
-	/* 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					AdminViewAllReviewPage frame = new AdminViewAllReviewPage();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-	*/
 
 	/**
 	 * Create the frame.
